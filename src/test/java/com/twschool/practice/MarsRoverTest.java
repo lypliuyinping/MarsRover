555cc8a540d73d873e78fc5f675f84c52737bac2
liuyinping <1689372042@qq.com>
2020-06-18 20:13:13 +0800

    should_return_coordinate_x0_y0_direction_E_given_coordinate_x0_y0_direction_N_command_R

--- src/test/java/com/twschool/practice/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -53,4 +53,16 @@ public class MarsRoverTest {
         Assert.assertEquals(0,marsRoverPostion.getcoordinateX());
         Assert.assertEquals(0,marsRoverPostion.getCoordinateY());
     }
+    @Test
+    public void  should_return_coordinate_x0_y0_direction_E_given_coordinate_x0_y0_direction_N_command_R(){
+        //given
+        MarsRoverPostion marsRoverPostion =new MarsRoverPostion(0,0,"N");
+        MarsRover marsRover =new MarsRover(marsRoverPostion);
+        //when
+        marsRover.receive("R");
+        //then
+        Assert.assertEquals("E",marsRoverPostion.getDirection());
+        Assert.assertEquals(0,marsRoverPostion.getcoordinateX());
+        Assert.assertEquals(0,marsRoverPostion.getCoordinateY());
+    }
 }
