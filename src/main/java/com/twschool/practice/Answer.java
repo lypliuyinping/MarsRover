04a2bd9d5862ead23f280ee3a6199e2a4199731e
liuyinping <1689372042@qq.com>
2020-06-19 17:30:08 +0800

    4A0B

--- src/main/java/com/twschool/practice/Answer.java
+++ src/main/java/com/twschool/practice/Answer.java
@@ -1,7 +1,26 @@
 package com.twschool.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Answer {
-    public static String check(int[] gameAnswer, int[] userAnswer) {
-        return "4A0B";
+    private final List<String> answerNumbers;
+
+    public Answer(String answerString) {
+        this.answerNumbers= Arrays.asList(answerString.split(" "));
+    }
+
+    public String check(String userAnswerString) {
+        List<String>  userAnswerNumber=Arrays.asList(userAnswerString.split(" "));
+        int allRight=0;
+        int partRight=0;
+        for(int i=0;i<answerNumbers.size();i++){
+            if(answerNumbers.get(i).equals(userAnswerNumber.get(i))){
+                allRight++;
+            }else if(answerNumbers.contains(userAnswerNumber.get(i))){
+                partRight++;
+            }
+        }
+         return allRight + "A" + partRight+"B";
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -7,11 +7,10 @@ public class GuessNumberTest {
     @Test
     public void  should_return_4_A_0_B_given_1_2_3_4(){
         //given
-       int[] gameAnswer={1,2,3,4};
-       int[] userAnswer={1,2,3,4};
+        Answer answer =new Answer("1 2 3 4");
 
         //when
-        String result = Answer.check(gameAnswer, userAnswer);
+        String result = answer.check("1 2 3 4");
         //then
 
        Assert.assertEquals("4A0B",result);
