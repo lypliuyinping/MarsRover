314ee92ce86e57a8c0c220b16d14097f5aba24dc
liuyinping <1689372042@qq.com>
2020-06-19 18:16:06 +0800

    should_return_1A3B_when_compare_with_1423_given_answer_1234

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class GuessNumberTest {
     @Test
-    public void  should_return_4_A_0_B_given_1_2_3_4(){
+    public void  should_return_4A0B_when_compare_with_1234_given_answer_1234(){
         //given
         Answer answer =new Answer("1 2 3 4");
 
@@ -15,4 +15,16 @@ public class GuessNumberTest {
 
        Assert.assertEquals("4A0B",result);
     }
+    @Test
+    public void  should_return_1A3B_when_compare_with_1423_given_answer_1234(){
+        //given
+        Answer answer =new Answer("1 2 3 4");
+
+        //when
+        String result = answer.check("1 4 2 3");
+        //then
+
+        Assert.assertEquals("1A3B",result);
+    }
+
 }
