01671d3dfe2e58ce9e57706c7ab587ee7c4a2cbf
liuyinping <1689372042@qq.com>
2020-06-19 11:22:26 +0800

    right

--- src/main/java/com/twschool/practice/Direction.java
+++ src/main/java/com/twschool/practice/Direction.java
@@ -6,9 +6,19 @@ public enum  Direction {
         public Direction leftDirection() {
             return Direction.N;
         }
+
+        @Override
+        public Direction rightDirection() {
+            return Direction.S;
+        }
     },S {
         @Override
         public Direction leftDirection() {
+            return Direction.W;
+        }
+
+        @Override
+        public Direction rightDirection() {
             return Direction.W;
         }
     },W {
@@ -16,13 +26,23 @@ public enum  Direction {
         public Direction leftDirection() {
             return Direction.S;
         }
+
+        @Override
+        public Direction rightDirection() {
+            return Direction.N;
+        }
     },N {
         @Override
         public Direction leftDirection() {
             return Direction.W;
         }
+
+        @Override
+        public Direction rightDirection() {
+            return Direction.E;
+        }
     };
     public abstract Direction leftDirection();
-
+    public abstract Direction rightDirection();
 }
 
